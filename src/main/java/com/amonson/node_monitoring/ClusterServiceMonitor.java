131f7e77bfd3dda71f5ad90a22546201dec41c6a
Paul Amonson <paulamonson@gmail.com>
2021-08-06 14:56:44 -0700

    [Feat] Added general communication to the ClusterServiceNode class.
    Signed-off-by: Paul Amonson <paulamonson@gmail.com>

--- src/main/java/com/amonson/node_monitoring/ClusterServiceMonitor.java
+++ src/main/java/com/amonson/node_monitoring/ClusterServiceMonitor.java
@@ -17,12 +17,10 @@ public class ClusterServiceMonitor {
      *
      * @param myHostname This nodes hostname.
      * @param clusterNodes All the hostnames in the cluster of services.
-     * @param callback Callback that receives either a primary or secondary state. Only one primary can exist
-     *                in the cluster at a time.
-     * @param logger Th native Java Logger to log messages.
+     * @param logger The native Java Logger to log messages.
      */
-    public ClusterServiceMonitor(String myHostname, String[] clusterNodes, RoleChange callback, Logger logger) {
-        this(myHostname, Arrays.asList(clusterNodes), callback, logger);
+    public ClusterServiceMonitor(String myHostname, String[] clusterNodes, Logger logger) {
+        this(myHostname, Arrays.asList(clusterNodes), logger);
     }
 
     /**
@@ -30,17 +28,14 @@ public class ClusterServiceMonitor {
      *
      * @param myHostname This nodes hostname.
      * @param clusterNodes All the hostnames in the cluster of services.
-     * @param callback Callback that receives either a primary or secondary state. Only one primary can exist
-     *                in the cluster at a time.
      * @param logger Th native Java Logger to log messages.
      */
-    public ClusterServiceMonitor(String myHostname, List<String> clusterNodes, RoleChange callback, Logger logger) {
+    public ClusterServiceMonitor(String myHostname, List<String> clusterNodes, Logger logger) {
         me_ = myHostname;
         for(String node: clusterNodes)
             nodes_.put(node, 0);
         nodes_.put(me_, rng_.nextInt(Integer.MAX_VALUE));
         primary_ = me_;
-        callback_ = callback;
         log_ = logger;
         monitoring_ = new NodeMonitoring(me_, nodes_.keySet(), this::callback, log_);
         evaluatePrimary(true);
@@ -56,13 +51,85 @@ public class ClusterServiceMonitor {
     }
 
     /**
+     * Sets the callback if the role matters in the cluster implementation.
+     *
+     * @param callback Callback that receives either a primary or secondary state. Only one primary can exist
+     *                in the cluster at a time.
+     */
+    public void setRoleCallback(RoleChange callback) {
+        callback_ = callback;
+    }
+
+    /**
+     * Sets the general message callback for node to node cluster communications.
+     */
+    public void setMessageCallback(MessageCallback callback) {
+        messageCallback_ = callback;
+        if(messageCallback_ == null)
+            monitoring_.removeHandler(MESSAGE);
+        else
+            monitoring_.addHandler(MESSAGE, this::messageCallback);
+    }
+
+    /**
+     * Send a message to a list of other nodes in the cluster.
+     *
+     * @param to Collection of nodes (not this node) to send the message to.
+     * @param message The message to send (format is defined by calling implementation).
+     */
+    public void sendMessage(Collection<String> to, String message) {
+        if(to == null) {
+            log_.fine("Attempt was made to send a message to no other nodes in the cluster.");
+            return;
+        }
+        if(message == null) {
+            log_.fine("Attempt was made to send a 'null' message.");
+            return;
+        }
+        Collection<String> leftAfterMe = new ArrayList<>(to);
+        leftAfterMe.remove(me_);
+        if(leftAfterMe.size() == 0) {
+            log_.fine("Attempt was made to send a message to no other nodes in the cluster.");
+            return;
+        }
+        String fullMessage = me_ + "|" + String.join(",", leftAfterMe) + "|" + message;
+        monitoring_.sendMessage(MESSAGE, fullMessage);
+    }
+
+    /**
+     * Send a message to all other nodes in cluster.
+     *
+     * @param message Message to send. A null message will not be sent.
+     */
+    public void sendMessageAll(String message) {
+        sendMessage(nodes_.keySet(), message);
+    }
+
+    /**
+     * Send a message to a Role in the cluster.
+     *
+     * @param to Role to send to, if null no message is sent.
+     * @param message The message to send (format is defined by calling implementation).
+     */
+    public void sendMessage(Role to, String message) {
+        if(to == null) {
+            log_.fine("Attempt was made to send a message to a 'null' Role.");
+            return;
+        }
+        if(to == Role.Primary)
+            sendMessage(getPrimaryNotMe(), message);
+        else
+            sendMessage(getSecondaryNotMe(), message);
+    }
+
+    /**
      * Join the cluster.
      *
      * @param blocking If set to true this method will block, if false the process will run in the background.
      */
     public void startMonitoring(boolean blocking) {
         if(!monitoring_.isRunning()) {
-            monitoring_.addHandler("ANNOUNCE", this::announceCallback);
+            monitoring_.addHandler(ANNOUNCE, this::announceCallback);
             monitoring_.startMonitoring();
             announce();
             if (blocking) {
@@ -118,8 +185,23 @@ public class ClusterServiceMonitor {
         void stateChanged(Role role);
     }
 
+    /**
+     * Defined callback for general message sending.
+     */
+    @FunctionalInterface public interface MessageCallback {
+        /**
+         * Called on a general incoming message.
+         *
+         * @param from The node name (hostname) of the sender of the message.
+         * @param to The node(s) (hostname(s)) the message was sent to. A '*' is to all nodes, a Role targets that Role
+         *           otherwise the node(s) are a comma seperated list of nodes.
+         * @param message The message payload. The format is undefined or defined by the caller implementation.
+         */
+        void incomingMessage(String from, List<String> to, String message);
+    }
+
     synchronized private void announce() {
-        monitoring_.sendMessage("ANNOUNCE", me_ + ":" + nodes_.get(me_));
+        monitoring_.sendMessage(ANNOUNCE, me_ + ":" + nodes_.get(me_));
     }
 
     private void callback(String node, boolean newState) {
@@ -128,6 +210,15 @@ public class ClusterServiceMonitor {
         else {
             nodes_.put(node, 0);
             evaluatePrimary(false);
+        }
+    }
+
+    private void messageCallback(String message) {
+        if(messageCallback_ != null) {
+            String[] parts = message.split("\\|");
+            String fixedMessage = String.join("|", Arrays.copyOfRange(parts, 2, parts.length));
+            List<String> toList = Arrays.asList(parts[1].split(","));
+            messageCallback_.incomingMessage(parts[0], toList, fixedMessage);
         }
     }
 
@@ -167,12 +258,29 @@ public class ClusterServiceMonitor {
             callback_.stateChanged(role);
     }
 
+    private List<String> getSecondaryNotMe() {
+        List<String> result = new ArrayList<>(nodes_.keySet());
+        result.remove(primary_);
+        return result;
+    }
+
+    private List<String> getPrimaryNotMe() {
+        List<String> result = new ArrayList<>();
+        if(!primary_.equals(me_))
+            result.add(primary_);
+        return result;
+    }
+
     private final Map<String, Integer> nodes_ = new HashMap<>();
     private final String me_;
     private final Logger log_;
     private final Random rng_ = new Random(Instant.now().toEpochMilli());
-    private final RoleChange callback_;
+    private       RoleChange callback_;
+    private       MessageCallback messageCallback_;
     private       NodeMonitoring monitoring_;
     private       String primary_;
     private       Role role_;
+
+    private static final String ANNOUNCE = "ANNOUNCE";
+    private static final String MESSAGE = "MESSAGE";
 }
--- src/test/groovy/com/amonson/node_monitoring/ClusterServiceMonitorSpec.groovy
+++ src/test/groovy/com/amonson/node_monitoring/ClusterServiceMonitorSpec.groovy
@@ -4,9 +4,13 @@ import spock.lang.Specification
 
 import java.util.logging.Logger
 
-class ClusterServiceMonitorSpec extends Specification implements ClusterServiceMonitor.RoleChange {
+class ClusterServiceMonitorSpec extends Specification implements ClusterServiceMonitor.RoleChange, ClusterServiceMonitor.MessageCallback {
     boolean running_
     int port_
+    String receivedMessage_
+    String topic_
+    String from_
+    Collection<String> to_
     def underTest_
 
     @Override
@@ -14,15 +18,25 @@ class ClusterServiceMonitorSpec extends Specification implements ClusterServiceM
     }
 
     void setup() {
+        topic_ = null
+        receivedMessage_ = null
+        from_ = null
+        to_ = null
         port_ = 9999
         running_ = false
-        underTest_ = new ClusterServiceMonitor("me", new String[] {"me", "other"}, this, Mock(Logger))
+        underTest_ = new ClusterServiceMonitor("me", new String[] {"me", "other"}, Mock(Logger))
+        underTest_.setRoleCallback(this)
         underTest_.monitoring_ = Mock(NodeMonitoring)
         underTest_.monitoring_.isRunning() >> { return running_ }
         underTest_.monitoring_.startMonitoring() >> { running_ = true }
         underTest_.monitoring_.stopMonitoring() >> { running_ = false }
         underTest_.role_ = ClusterServiceMonitor.Role.Primary
         underTest_.monitoring_.setPort(_ as Integer) >> { Integer port -> this.port_ = port }
+        underTest_.monitoring_.sendMessage(_ as String, _ as String) >> {
+            String topic, String msg ->
+                topic_ = topic
+                receivedMessage_ = msg
+        }
     }
 
     def "Test non-blocking"() {
@@ -65,7 +79,7 @@ class ClusterServiceMonitorSpec extends Specification implements ClusterServiceM
     }
 
     def "Test no callback"() {
-        underTest_ = new ClusterServiceMonitor("me", new String[] {"me", "other"}, null, Mock(Logger))
+        underTest_ = new ClusterServiceMonitor("me", new String[] {"me", "other"}, Mock(Logger))
         underTest_.monitoring_ = Mock(NodeMonitoring)
         underTest_.monitoring_.isRunning() >> { return running_ }
         underTest_.monitoring_.startMonitoring() >> { running_ = true }
@@ -77,5 +91,71 @@ class ClusterServiceMonitorSpec extends Specification implements ClusterServiceM
         underTest_.announceCallback(String.format("other:%d", 0))
         expect: before == ClusterServiceMonitor.Role.Secondary
         and:    underTest_.getRole() == ClusterServiceMonitor.Role.Primary
+    }
+
+    def "Test sendMessageAll"() {
+        underTest_.sendMessageAll("message")
+        expect: receivedMessage_ == "me|other|message"
+        and: topic_ == "MESSAGE"
+    }
+
+    def "Test sendMessage for Primary"() {
+        underTest_.sendMessage(ClusterServiceMonitor.Role.Primary, "message")
+        expect: receivedMessage_ == null
+        and: topic_ == null
+    }
+
+    def "Test sendMessage for Secondary"() {
+        underTest_.sendMessage(ClusterServiceMonitor.Role.Secondary, "message")
+        expect: receivedMessage_ == "me|other|message"
+        and: topic_ == "MESSAGE"
+    }
+
+    def "Test sendMessage for Primary 2"() {
+        underTest_.role_ = ClusterServiceMonitor.Role.Secondary
+        underTest_.primary_ = "other"
+        underTest_.sendMessage(ClusterServiceMonitor.Role.Primary, "message")
+        expect: receivedMessage_ == "me|other|message"
+        and: topic_ == "MESSAGE"
+    }
+
+    def "Test sendMessage for null Role"() {
+        underTest_.sendMessage((ClusterServiceMonitor.Role)null, "message")
+        expect: receivedMessage_ == null
+        and: topic_ == null
+    }
+
+    def "Test sendMessage for null Collection"() {
+        underTest_.sendMessage((Collection<String>)null, "message")
+        expect: receivedMessage_ == null
+        and: topic_ == null
+    }
+
+    def "Test sendMessage for Collection null message"() {
+        underTest_.sendMessage(["other"], null)
+        expect: receivedMessage_ == null
+        and: topic_ == null
+    }
+
+    @Override
+    void incomingMessage(String from, List<String> to, String message) {
+        from_ = from
+        to_ = to
+        receivedMessage_ = message
+    }
+    def "Test messageCallback"() {
+        underTest_.setMessageCallback(this)
+        underTest_.messageCallback("me|other|message")
+        expect: from_ == "me"
+        and:    receivedMessage_ == "message"
+        and:    to_.size() == 1
+        and:    to_[0] == "other"
+    }
+
+    def "Test messageCallback 2"() {
+        underTest_.setMessageCallback(this)
+        underTest_.setMessageCallback(null)
+        underTest_.messageCallback("me|other|message")
+        expect: from_ == null
     }
 }
